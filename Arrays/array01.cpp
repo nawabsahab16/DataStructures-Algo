9e9cd8b1de9e3908b52d9c1acb9a54175439564c
nawabsahab16 <117763400+nawabsahab16@users.noreply.github.com>
2023-03-11 19:53:52 +0530

    Program of an array to demonstrate Different operations such as update ,create etc..

---
+++ Arrays/array01.cpp
@@ -0,0 +1,32 @@
+#include<iostream>
+using namespace std;
+int main() {
+    int marks[100];
+
+    int n;
+    cout << "Enter the number of students ";
+    cin>>n;
+
+    // Assign value 
+
+    marks[0] = -1;
+
+    // Input 
+
+    for(int i = 1; i<n; i++){
+        cin>> marks[i];
+   
+   // Update 
+       marks[i]= marks[i]*2;  
+    }
+
+    for(int i=1; i<=n ; i++){
+        cout << marks[i]<< " , " ;
+
+    }
+
+    cout << endl;
+
+    return 0;
+    
+}

182c92dc52423883265cd3751b5480c151d59cda
nawabsahab16 <117763400+nawabsahab16@users.noreply.github.com>
2023-03-14 18:50:27 +0530

    Program to find LowerBound Most optimised

--- ArraysExercise/LowerBound.cc
+++ ArraysExercise/LowerBound.cc
@@ -1,25 +1,28 @@
 #include<iostream>
 using namespace std;
 
+
 int LowerBound(vector<int> A, int Val) {
     int sz= A.size();
-
     int l=0, r=(sz-1);
     int answer = -1;
+
 
   while(1<= r) {
     int mid = (1+r)/2;
     if(a[mid]>Val){
         r = mid - 1;
-
     } 
+
+
     else {
         answer = a[mid];
         l += mid;
 
 
     }
-  }
 
-  return answer;
+
+  }
+   return answer;
 }

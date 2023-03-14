5e04ad02adbd6eca696911531dcf5da648fbcc3d
nawabsahab16 <117763400+nawabsahab16@users.noreply.github.com>
2023-03-14 18:42:55 +0530

    Program to find MaxSum OF Subarray O(N)

---
+++ ArraysExercise/MaxSumSubarr.cc
@@ -0,0 +1,31 @@
+#include<iostream>
+using namespace std;
+
+
+int MaxSumSubArr(vector<int> A) {
+    boolf = true;
+
+    int rr= INT_MIN;
+
+    for(auto x : A) {
+
+       if(x > 0) f = false;
+       if(x > rr)rr =  x;
+
+    }
+
+    if(f) return rr;
+        int MaxSumFromPrevIndexTOLeft = 0;
+
+        int finalAnswer = 0;
+        int temporary = 0;
+
+        int arraySize = A.size();
+
+        for(int i=0; i<arraySize; i++)
+ {
+      temporary = MaxSumFromPrevIndexTOLeft + A[i];
+ }
+ 
+ 
+ }

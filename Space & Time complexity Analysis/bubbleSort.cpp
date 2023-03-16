e7cfa3185dbbe9db1525936a0419f9752624033e
nawabsahab16 <117763400+nawabsahab16@users.noreply.github.com>
2023-03-16 17:05:35 +0530

    Space&Time complexity analysis for Bubble Sort code(N*N)

---
+++ Space & Time complexity Analysis/bubbleSort.cpp
@@ -0,0 +1,39 @@
+#include<iostream>
+#include<vector>
+#include<ctime>
+#include<algorithm>
+using namespace std;
+
+bool compare(int a, int b){
+    return a>b;
+}
+
+void BubbleSort(vector<int> &a, int n){
+    for(int times=1; times<= n-1; times++) {
+        for(int j=0; j<n-times; j++){
+            if(compare(a[j], a[j+1])){
+                swap(a[j], a[j+1]);
+            }
+        }
+    }
+}
+
+
+int main() {
+    int key;
+    cin>>key;
+
+    vector<int> arr(n,0);
+
+    for(int i=0; i<n; i++) {
+        arr[i]= n-1;
+    }
+
+    auto start_time = clock();
+    BubbleSort(arr, n);
+    auto end_time = clock();
+
+    cout << end_time - start_time << endl;
+
+    return 0;
+}

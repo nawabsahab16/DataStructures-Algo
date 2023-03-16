e8ceb3b818fc23eb581e1ba6999c4be01c7907f0
nawabsahab16 <117763400+nawabsahab16@users.noreply.github.com>
2023-03-16 16:55:54 +0530

    exercise code

---
+++ ArraysExercise/K_Rotate.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+
+vector<int> v;
+int n = a.size();
+
+K = K % n;
+for(int i =0; i<n; i++) {
+    if(i < K ){
+        v.push_back(a[n + i - K ]);
+
+    }
+
+    v.push_back([a[i]- K]);
+}
+
+return v;
